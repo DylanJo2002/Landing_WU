79173bd89f6ea9d26ea74f6a04a8de6c26c34d9c
Dilan Joel Bergano Caicedo <=>
2022-05-02 16:05:25 -0500

    Sección 1, Te Conectamos, creada

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderFooter from '../HeaderFooter/Header'
+import SectionTeConectamos from '../SectionTeConectamos/SectionTeConectamos';
 
 function App() {
   return (
     <div className="App">
       <HeaderFooter type="header"></HeaderFooter>
+      <SectionTeConectamos></SectionTeConectamos>
       <HeaderFooter type="footer"></HeaderFooter>
     </div>
   );
---
+++ src/components/SectionTeConectamos/SectionTeConectamos.js
@@ -0,0 +1,37 @@
+import './SectionTeConectamos.css';
+import logoWU from './img/logo_wu.png'
+import persona from './img/persona_dinero.png'
+
+function SectionTeConectamos() {
+  return (
+    <div className='soft_grey my-0'>
+      <div className='container'>
+
+        <div className="row">
+          <div className="col-6 d-flex flex-column justify-content-center">
+            <div>
+              <img src={logoWU} alt="Logo de Western Union"/>
+            </div>
+            <div>
+              <p className='black'><span className='h1 museoSans'>Te conectamos</span>
+                <br/>
+                <span className='h3 museoSans700'>con más de 200 países a través
+                  <br/>del</span> <span className='h2 museoSans'>Cobro y Envío de
+                  <br/>Giros Internacionales</span>
+              </p>
+
+            </div>
+          </div>
+
+          <div className="col-6">
+            <img src={persona} alt="Persona con computador y dinero"/>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default SectionTeConectamos;

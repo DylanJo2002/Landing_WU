e0122a01c9474c8f18c4bfc8b8d1b5d65a6354ca
Dilan Joel Bergano Caicedo <=>
2022-05-02 11:48:24 -0500

    Dependencias de bootstrap añadidas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+/*Bootstrap importations*/
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

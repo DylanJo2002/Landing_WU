836357716bdcb1209b953f0af874e7f2d627d70b
Dilan Joel Bergano Caicedo <=>
2022-05-02 21:45:28 -0500

    Diseño responsive tanto de escritorio como movil del header, footer y la primera sección, Te Conectamos

--- src/components/ContentHeaderFooter/ContentHeaderFooter.js
+++ src/components/ContentHeaderFooter/ContentHeaderFooter.js
@@ -4,10 +4,14 @@ import './ContentHeaderFooter.css';
 
 function ContentHeaderFooter() {
     return (
-        <div className="container my-0 p-0">
-            <div className="d-flex justify-content-between align-items-center">
-                <img className="vigilado" src={vigilado} alt="Vigilado Superintendencia Financiera de Colombia"/>
-                <img className="logo" src={logo} alt="logo de Giros y Finanzas, y Western Union"/>
+        <div className="container my-0 p-sm-0">
+            <div className="d-flex justify-content-sm-between align-items-center">
+                <div>
+                    <img className="vigilado" src={vigilado} alt="Vigilado Superintendencia Financiera de Colombia"/>
+                </div>
+                <div>
+                    <img className="logo" src={logo} alt="logo de Giros y Finanzas, y Western Union"/>
+                </div>
             </div>
         </div>
     );
--- src/components/SectionTeConectamos/SectionTeConectamos.js
+++ src/components/SectionTeConectamos/SectionTeConectamos.js
@@ -7,10 +7,11 @@ function SectionTeConectamos() {
     <div className='soft_grey my-0'>
       <div className='container'>
 
-        <div className="row">
-          <div className="col-6 d-flex flex-column justify-content-center">
+        <div className="row m-0">
+          <div className="col-12 col-lg-6 d-flex flex-column justify-content-center
+           align-items-center align-items-lg-stretch p-0">
             <div>
-              <img src={logoWU} alt="Logo de Western Union"/>
+              <img className='img-fluid' src={logoWU} alt="Logo de Western Union"/>
             </div>
             <div>
               <p className='black'><span className='h1 museoSans'>Te conectamos</span>
@@ -23,8 +24,8 @@ function SectionTeConectamos() {
             </div>
           </div>
 
-          <div className="col-6">
-            <img className='imgPersona' src={persona} alt="Persona con computador y dinero"/>
+          <div className="col-12 col-lg-6 p-0">
+            <img className='imgPersona img-fluid' src={persona} alt="Persona con computador y dinero"/>
           </div>
 
         </div>
